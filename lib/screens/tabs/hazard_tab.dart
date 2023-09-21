8de8d05943ce01b61d96f27990ff81cb058ec1ba
Lans12345 <olanalans12345@gmail.com>
2023-09-21 20:02:38 +0800

    fetched announcements

--- lib/screens/tabs/hazard_tab.dart
+++ lib/screens/tabs/hazard_tab.dart
@@ -1,31 +1,65 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:responder/widgets/text_widget.dart';
+import 'package:intl/intl.dart';
 
 class HazardTab extends StatelessWidget {
   const HazardTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return ListTile(
-          leading: const Icon(
-            Icons.info,
-          ),
-          title: TextWidget(
-            text: 'Title of the Announcements',
-            fontSize: 18,
-            color: Colors.black,
-            fontFamily: 'Bold',
-          ),
-          subtitle: TextWidget(
-            text: 'Description of the Announcements',
-            fontSize: 12,
-            color: Colors.grey,
-          ),
-        );
-      },
-    );
+    return StreamBuilder<QuerySnapshot>(
+        stream:
+            FirebaseFirestore.instance.collection('Announcements').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            print(snapshot.error);
+            return const Center(child: Text('Error'));
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            print('waiting');
+            return const Padding(
+              padding: EdgeInsets.only(top: 50),
+              child: Center(
+                  child: CircularProgressIndicator(
+                color: Colors.black,
+              )),
+            );
+          }
+
+          final data = snapshot.requireData;
+          return SizedBox(
+            width: 400,
+            height: 600,
+            child: ListView.builder(
+              itemCount: data.docs.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                    leading: const Icon(
+                      Icons.info,
+                    ),
+                    title: TextWidget(
+                      text: data.docs[index]['name'],
+                      fontSize: 18,
+                      color: Colors.black,
+                      fontFamily: 'Bold',
+                    ),
+                    subtitle: TextWidget(
+                      text: data.docs[index]['desc'],
+                      fontSize: 12,
+                      color: Colors.grey,
+                    ),
+                    trailing: TextWidget(
+                      text: DateFormat.yMMMd()
+                          .add_jm()
+                          .format(data.docs[index]['dateTime'].toDate()),
+                      fontSize: 12,
+                      color: Colors.grey,
+                    ));
+              },
+            ),
+          );
+        });
   }
 
   Widget cardWidget(String path, String number) {
